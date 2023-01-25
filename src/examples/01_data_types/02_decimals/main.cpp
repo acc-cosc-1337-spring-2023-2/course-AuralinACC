a31ef3a88ecfc6583b3d5717ac89e5c36778e7fe
AuralinACC <123131856+AuralinACC@users.noreply.github.com>
2023-01-25 01:09:19 +0000

    Intro to unit tests, and data types

--- src/examples/01_data_types/02_decimals/main.cpp
+++ src/examples/01_data_types/02_decimals/main.cpp
@@ -1,5 +1,18 @@
+#include "decimals.h" //tell C++ where to find add to double 1 function
+
+#include<iostream>//we want to use cout
+
+using std::cout;//explicitly tells c++ that we are only using cout from iostream
+
 int main()
 {
+    double num = 0;
+    double num1 = add_to_double_1(num);
+
+    //we do something
+    num1 = num1 + 1
+
+    cout<<"Adding to a double: "<<num1<<"\n";
 
     return 0;
 }
--- src/examples/01_data_types/03_auto/main.cpp
+++ src/examples/01_data_types/03_auto/main.cpp
@@ -1,5 +1,14 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    auto num = 5;//int who number type
+    auto dec = 5.5;//double
+
+    cout<<"Num is integer: "<<num<<"\n";
+    cout<<"Dec is Decimal: "<<dec<<"\n";
 
     return 0;
 }
