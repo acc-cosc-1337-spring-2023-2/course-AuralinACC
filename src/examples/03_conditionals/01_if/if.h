139d3b4cf1c278b8db7e6701f6e425ea93ade2bf
AuralinACC <123131856+AuralinACC@users.noreply.github.com>
2023-02-10 01:33:18 +0000

    class feb 9

--- src/examples/03_conditionals/01_if/if.h
+++ src/examples/03_conditionals/01_if/if.h
@@ -4,3 +4,8 @@
 bool get_anded_result(bool b1, bool b2);
 bool get_ored_result(bool b1, bool b2);
 bool get_notted_result(bool b1);
+bool get_order_of_ops(bool b1, bool b2, bool b3);
+
+//boolean expresions == !== <= >= > < __> boolean results
+bool is_overtime(int hours);
+bool is_even(int hours);
