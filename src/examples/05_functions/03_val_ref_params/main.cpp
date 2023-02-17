e63c8e13345881c0ae2114b04a0316b77bc7c43a
AuralinACC <123131856+AuralinACC@users.noreply.github.com>
2023-02-17 00:50:04 +0000

    class Feb 16

--- src/examples/05_functions/03_val_ref_params/main.cpp
+++ src/examples/05_functions/03_val_ref_params/main.cpp
@@ -1,4 +1,21 @@
+#include<iostream>
+
+using std::cout; using std::cin;
+
 int main()
 {
+    auto num = 10;
+    int &num_ref = num;//num_ref will store the address of num
+
+    cout<<"Num is:"<<num<<"\n";
+    cout<<"Stack address for num is: "<<&num<<"\n";
+
+    cout<<"num value via num_ref: "<<num_ref<<"\n";
+    cout<<"Address that num_ref points to: "<<&num_ref<<"\n\n";
+
+    num_ref = 20; //?? will this change the value of num?
+    cout<<"Num is:"<<num<<"\n";
+    cout<<"Stack address for num is: "<<&num<<"\n";
+
     return 0;
 }
