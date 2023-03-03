c73f5cb3cc29c9b5f7d02691299b6af892cc9e3c
AuralinACC <123131856+AuralinACC@users.noreply.github.com>
2023-03-03 19:52:55 +0000

    HW 5 submission

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,56 @@
 //add include statements
+#include "func.h"
 
 //add function code here
+double get_gc_content(const string& dna)
+{
+    int gc_count = 0;
+    for (std::string::size_type i = 0; i < dna.length(); i++)
+    {
+        if (dna[i] == 'G' || dna[i] == 'C' )
+        {
+            gc_count++;
+        }
+    }
+    double gc_content = (double) gc_count / dna.length();
+    return gc_content;
+}
+
+string get_dna_complement(string dna)
+{
+    dna = reverse_string(dna);
+    int len = dna.length();
+    for (int i = 0; i < len; i++)
+    {
+        if (dna[i] == 'A')
+        {
+            dna[i] = 'T';
+        }
+        else if (dna[i] == 'T')
+        {
+            dna[i] = 'A';
+        }
+        else if (dna[i] == 'C')
+        {
+            dna[i] = 'G';
+        }
+        else if (dna[i] == 'G')
+        {
+            dna[i] = 'C';
+        }
+        
+    }
+    return dna;
+}
+
+string reverse_string(string dna)
+{
+    int string_length = dna.length();
+    for (int i = 0; i < string_length / 2; i++)
+    {
+        char temp = dna[i];
+        dna[i] = dna[string_length - i - 1];
+        dna[string_length - i - 1] = temp;
+    }
+    return dna;
+}
--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,2 +1,10 @@
+#include<iostream>
+#include<string>
+
+using std::string;
+
 //add function(s) prototype here
+
 double get_gc_content(const string& dna);
+string get_dna_complement(string dna);
+string reverse_string(string dna);
