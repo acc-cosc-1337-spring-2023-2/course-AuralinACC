f5a64a191a79c91a68eb092b8b19f5b8aa467173
AuralinACC <123131856+AuralinACC@users.noreply.github.com>
2023-03-25 04:13:45 +0000

    HW05 main WiP

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,26 @@
+#include "tic_tac_toe.h"
 
 int main() 
 {
+    TicTacToe game; //create an instance of the class
+    
+    //1) Prompt the user for first player
+    string first_player;
+    while(first_player != "X" && first_player != "O")
+    {
+        cout<<"Enter X or O: ";
+        cin>>first_player;
+    }
+    
+    //2) Start the game
+    game.start_game(first_player);
+
+    //3) Create a user controlled loop to play the game until user opts to quit.
+    while(game.game_over() == false)
+    {
+        cout<<"Enter position from 1 to 9: ";
+    }
+    
 
 	return 0;
 }
