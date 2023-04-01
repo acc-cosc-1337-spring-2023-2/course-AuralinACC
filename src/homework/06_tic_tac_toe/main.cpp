e284f7a096dd7e263254a9f5206e5d95d15361cc
AuralinACC <123131856+AuralinACC@users.noreply.github.com>
2023-04-01 02:47:44 +0000

    Homework 7 submission

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -4,38 +4,63 @@ int main()
 {
     TicTacToe game; //create an instance of the class
 
-    auto prompt = 'i';
+    auto prompt = 'Y';
 
-    do
+    while (prompt == 'Y' || prompt == 'y')
     {
         //1) Prompt the user for first player
         string first_player;
         int position;
-        while(first_player != "X" && first_player != "O")
+        while(true)
         {
             cout<<"Enter X or O: ";
             cin>>first_player;
+            if(first_player == "X" || first_player == "O")
+            {
+                break;
+            }
+            else
+            {
+                cout<<"Invalid input. Please enter X or O.\n";
+            }
         }
-        
         //2) Start the game
         game.start_game(first_player);
 
-        //3) Create a user controlled loop to play the game until user opts to quit.
-        //4) In an inner loop while game is not over, prompt the user for a position 
-        //(int type) and call the mark_board tic tac toe class member function. 
+            //3) Create a user controlled loop to play the game until user opts to quit.
+            //4) In an inner loop while game is not over, prompt the user for a position 
+            //(int type) and call the mark_board tic tac toe class member function. 
         while(game.game_over() == false)
-        {
-            cout<<"Enter position from 1 to 9: ";
-            cin>>position;
+        {   
+            while(true)
+            {
+                cout<<"Enter position from 1 to 9: ";
+                cin>>position;
+                if(position >= 1 && position <=9)
+                {
+                    break;
+                }
+                else
+                {
+                    cout<<"Invalid input.  Please enter a number from 1 to 9. \n";
+                }
+            }
             game.mark_board(position);
-            game.display_board();
+            game.display_board(); 
         }
-        cout<<"Game over\n\n";
+        cout<<"Game over\n"<<"The winner is "<<game.get_winner()<<".\n\n";
 
-        cout<<"Would you like to play another game? (Y/N) ";
-        cin>>prompt;
-    } 
-    while (prompt != 'n' && prompt != 'N');
-
+        while (true)
+        {
+            cout<<"Would you like to play another game? (Y/N) ";
+            cin>>prompt;
+        
+            if (prompt == 'n' || prompt == 'N' || prompt == 'y' || prompt == 'Y')
+            {
+                break;
+            }
+            else{cout<<"Invalid input. Please enter a Y or N/\n";}
+        }
+    }
 	return 0;
 }
