3476e84fb75e4f7ce96236482821e65931623483
AuralinACC <123131856+AuralinACC@users.noreply.github.com>
2023-04-11 22:31:27 +0000

    arrays_mem update

--- src/examples/10_arrays/01_arrays_mem/arrays_mem.cpp
+++ src/examples/10_arrays/01_arrays_mem/arrays_mem.cpp
@@ -1,15 +1,14 @@
 //write code for for stack_array and display each element to screen
 #include "arrays_mem.h"
-#include <iostream>
 
 using std::cout; using std::string;
 
 void use_stack_array()
 {
     const auto SIZE = 3;
-    int numbers[SIZE]{5, 8, 0}; //creates a list of three elements
+    int numbers[SIZE]{5, 8, 0};//creates a list of three elements
 
-    for(int i=0; i < SIZE; i++)
+    for(int i=0; i < SIZE; i++)   
     {
         cout<<numbers[i]<<"\n";
     }
@@ -21,14 +20,15 @@ void use_stack_array()
 void array_months()
 {
     const auto SIZE = 3;
-    int month_numbers[3]{1,2,3};
-    string month_names[SIZE] {"Jan", "Feb", "Mar"}; 
+    int month_numbers[SIZE]{1, 2, 3};
+    string month_names[SIZE] {"Jan", "Feb", "Mar"};
 
     for(auto i=0; i < SIZE; i++)
     {
         cout<<month_numbers[i]<<" "<<month_names[i]<<"\n";
-    }         
-    month_names[0] = "January"; //the string class knows how to update itself
+    }
+
+    month_names[0] = "January";//the string class knows how to update itself
     cout<<month_names[0]<<"\n";
 }
 
@@ -40,7 +40,7 @@ void array_and_pointers()
     cout<<*numbers<<"\n";
 
     int* ptr = numbers;
-    cout<<ptr<<"\n";
+    cout<<*ptr<<"\n";
 
     *ptr++;//legal? YES->
     cout<<*ptr<<"\n";
@@ -54,16 +54,16 @@ void array_and_pointers()
 
 void display_array(int *nums)
 {
-    for(auto i=0; nums[i] != -1; i ++)//sentinel value
+    for(auto i=0; nums[i] != -1; i++)//sentinel value
     {
         cout<<nums[i]<<"\n";
     }
 }
 
-void display_array(int * nums, const int SIZE)
+void display_array(int *nums, const int SIZE)
 {
     for(auto i=0; i < SIZE; i++)
     {
-        cout<< nums<<"\n";
+        cout<<nums[i]<<"\n";
     }
 }
--- src/examples/10_arrays/01_arrays_mem/main.cpp
+++ src/examples/10_arrays/01_arrays_mem/main.cpp
@@ -2,16 +2,15 @@
 
 int main() 
 {
-	use_stack_array();
-	array_months();
-	array_and_pointers();
+	//use_stack_array();
+	//array_months();
+	//array_and_pointers();
 
 	const int SIZE = 4;
 	int numbers[SIZE] {5, 8, 0, -1};
 
 	//display_array(numbers);
 	display_array(numbers, SIZE);
-
 	return 0;
 
 }
