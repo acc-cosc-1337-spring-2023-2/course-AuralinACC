c05960e27662a0e5eb303c2bb3cdb131144a3f46
AuralinACC <123131856+AuralinACC@users.noreply.github.com>
2023-04-12 16:10:58 +0000

    class arrays WIP

--- src/examples/10_arrays/04_arrays_pointers/arrays_pointers.cpp
+++ src/examples/10_arrays/04_arrays_pointers/arrays_pointers.cpp
@@ -1,1 +1,45 @@
 //cpp
+#include "arrays_pointers.h"
+
+ArrayList::ArrayList(int* list, const int capacity) : elements(list)
+{
+    this->capacity = capacity;
+}
+
+void ArrayList::Add(int value)
+{
+    if ( current_index < capacity)
+    {
+        elements[current_index] = value;
+        current_index++;
+    }
+}
+
+int ArrayList::Get_first_Index_Of(int value)
+{
+    auto index = -1;
+    auto local_current_index = 0;
+
+    while(local_current_index < current_index)
+    {
+        if(elements[local_current_index] == value)
+        {
+            index = local_current_index;
+            break;
+        }
+        local_current_index++;
+    }
+
+    return index;
+}
+void ArrayList::Delete(int value)
+{
+    auto index = Get_first_Index_Of(value);
+
+    AdjustElements(index);
+}
+
+void ArrayList::AdjustElements(int index)
+{
+    
+}
--- test/examples/10_arrays/10_arrays_tests.cpp
+++ test/examples/10_arrays/10_arrays_tests.cpp
@@ -1,6 +1,40 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "arrays_pointers.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+TEST_CASE("Test array list capacity")
+{
+	const auto SIZE = 5;
+	int numbers[SIZE];
+
+	ArrayList array_list(numbers, SIZE);
+
+	REQUIRE(array_list.Capacity() == 5);
+}
+TEST_CASE("Test array list size with empty list")
+{
+	const auto SIZE = 5;
+	int numbers[SIZE];
+
+	ArrayList array_list(numbers, SIZE);
+
+	REQUIRE(array_list.Size() == 0);
+}
+
+TEST_CASE("Test array list size with added elements")
+{
+	const auto SIZE = 5;
+	int numbers[SIZE];
+
+	ArrayList array_list(numbers, SIZE);
+
+	REQUIRE(array_list.Size() == 0);
+
+	array_list.Add(7);
+	REQUIRE(array_list.Size() == 1);
+	array_list.Add(8);
+	REQUIRE(array_list.Size() == 2);
 }
