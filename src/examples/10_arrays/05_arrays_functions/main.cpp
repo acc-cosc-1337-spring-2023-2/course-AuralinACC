6237b028cce1da9008cf29ad6afd280b38bea4ac
AuralinACC <123131856+AuralinACC@users.noreply.github.com>
2023-04-13 22:55:02 +0000

    create dynamic for one int

--- src/examples/10_arrays/05_arrays_functions/main.cpp
+++ src/examples/10_arrays/05_arrays_functions/main.cpp
@@ -1,8 +1,19 @@
 #include "arr_functions.h"
+#include <iostream>
+
+using std::cout;
+
 
 int main() 
 {
-	
+	//create dynamic memory
+	int* num = new int(5);//creates dynamic memory; 5 is in heap memory
+
+	//use the memrory(data)-limit the scope
+	cout<<*num<<"\n";
+
+	//delete memory
+	delete num;
 
 	return 0;
 }
